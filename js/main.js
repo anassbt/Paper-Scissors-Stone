50374c3574fb449cd893db6ea0c3467587cab2bf
anassbt <anass.boumssimrat@gmail.com>
2017-07-11 11:03:21 +0200

    Modification in the initial game, delete all the console.log, prompt, alert...

--- js/main.js
+++ js/main.js
@@ -1,118 +1,90 @@
 // START JAVASCRIPT PAPER, STONE, SCISSORS
 
-/* var compare = function (choix1, choix2)
-{
-    if (choix1 === choix2)
-    {
-        return "Egalité!";
-    }
-    else
-    {
-        if(choix1 === "Pierre")
-        {
-            if(choix2 === "Feuille")
-            {
-               return "La Feuille bat la Pierre. Vous avez perdu.";
-            }
-            else
-            {
-                return "La Pierre bat le Ciseau. Vous avez gagné.";
+var joueur = 0;
+var Ordi = 0;
 
-            }
-        }
-        else
-        {
-            if(choix1 === "Feuille")
-                {
-                     if(choice2 === "Pierre")
-                        {
-                             return "La Feuille bat la Pierre. Vous avez gagné.";
-                        }
-            else
-                {
-                return "Le Ciseau bat la Pierre. Vous avez perdu.";               }
-
-                }
-    if(choix1 === "Ciseau")
-                {
-                     if(choix2 === "Pierre")
-                        {
-                             return "La Pierre bat le Ciseau. Vous avez perdu.";
-                        }
-            else
-                {
-                return "Le Ciseau bat la Pierre. Vous avez gagné.";               }
-
-                }
-        }
-    }
-
-
-
-};
-var r = function(user)
-{
-    while(user < 0 | user >3)
-    {user = prompt("Pierre, Feuille, Ciseaux ?");
-    }
-
-    if(user === "Pierre")
-    user = "Pierre";
-
-    else
-    {
-        if(user === "Feuille")
-        {user = "Pierre";}
-        else
-        {user = "Ciseau";}
-    };
-    console.log("Vous avez choisi: " + user);
-
-    computerChoice = Math.random()
-    if(computerChoice <= 0.33)
-    {
-        computerChoice = "Pierre";
-    }
-    else
-    {
-        if(computerChoice > 0.33 && computerChoice <=0.66)
-        {computerChoice = "Pierre";}
-        else
-        {computerChoice = "Ciseau";}
-    }
-
-    console.log("ordinateur choisi: "+computerChoice)
-    console.log(compare(user, computerChoice));
-    if(user===computerChoice)
-    {
-        userChoice = user;
-        return "Pierre";}
-
-};
-
-
-var userChoice = prompt("Pierre, Feuille, Ciseaux")
-var computerChoice;
-
-var a = r(userChoice);
-if(a === "Pierre")
-{//console.log("Pierre");
-while(userChoice === computerChoice)
-{
-    var a = prompt("Egalité. Rejouons! Pierre, Feuille, Ciseaux.")
-    var b = r(a);
-    if(b !== "Pierre")
-    {break;}
+var choixuser="";
+function choix(x){
+  choixuser=x;
+  document.getElementById('demo').innerHTML =  choixuser;
+  game();
 }
-} */
+function game(choix) {
+  a=choixuser;
 
-// END JAVASCRIPT PAPER, STONE, SCISSORS
+if (joueur < 3 && Ordi < 3 ){
 
-// NOM DU JOUEUR
+var b = Math.floor((Math.random() * 3) + 1);
+
+
+
+if(b==1){
+  b="pierre";
+} else if (b==2){
+   b="papier";
+} else {
+  b="ciseaux";
+}
+
+document.getElementById('demo').innerHTML = "L'Ordinateur à choisi " + "" + b;
+
+if(a==b){
+  alert("égalité");
+}
+else if(a==="pierre" && b==="papier"){
+
+Ordi++;
+}
+else if(a==="ciseaux" && b==="papier"){
+
+joueur++;
+}
+else if(a==="papier" && b==="pierre"){
+
+joueur++;
+}
+else if(a==="papier" && b==="ciseaux"){
+
+Ordi++;
+}
+else if(a==="ciseaux" && b==="pierre"){
+
+Ordi++;
+}
+else if(a==="ciseaux" && b==="papier"){
+
+joueur++;
+}
+
+document.getElementById("joueur").innerHTML = joueur;
+document.getElementById("Ordi").innerHTML = Ordi;
+
+if (joueur == 3) {
+  document.getElementById("goku").src = "img/5934fdaa8e0f7833128f0e8dc704bc1f.png";
+;
+}
+else if(Ordi==3){
+  document.getElementById("win").src = "img/vegeta_ssj_by_naironkr-dadgz00.png";
+}
+
+
+}
+
+
+}
+
+
+//END JAVASCRIPT PAPER, STONE, SCISSORS
+
+
+//START PLAYER NAME
 
 function myFunction() {
   var x = document.getElementById("fname").value;
   document.getElementById("result").innerHTML = "On va jouer à un jeu" + ", " + x;
-document.getElementById("playername").innerHTML = " => " + x;
-document.getElementById("scoreplayername").innerHTML = " => " + x;
+  document.getElementById("playername").innerHTML = "" + x;
+  document.getElementById("scoreplayername").innerHTML = "" + x;
+  document.getElementById("helloplayername").innerHTML = "Bonjour " + x;
 }
+
+// END PLAYER NAME
